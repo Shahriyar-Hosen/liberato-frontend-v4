80e32e1d38073ca09750e20446c0bf8a05a84b62
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-03 11:07:11 +0600

    update home

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -26,13 +26,13 @@ export default async function Page({ params: { lng } }: IParamsLng) {
       <main>
         <Header heading={t('h1')} />
         <h2>
-          <Trans t={t} i18nKey="welcome">
+          <Trans t={t} i18nKey='welcome'>
             Welcome to Next.js v13 <small>appDir</small> and i18next
           </Trans>
         </h2>
         <div style={{ width: '100%' }}>
           <p>
-            <Trans t={t} i18nKey="blog.text">
+            <Trans t={t} i18nKey='blog.text'>
               Check out the corresponding <a href={t('blog.link')}>blog post</a>{' '}
               describing this example.
             </Trans>
@@ -40,18 +40,18 @@ export default async function Page({ params: { lng } }: IParamsLng) {
           <a href={t('blog.link')}>
             <img
               style={{ width: '50%' }}
-              src="https://locize.com/blog/next-13-app-dir-i18n/next-13-app-dir-i18n.jpg"
-              alt=""
+              src='https://locize.com/blog/next-13-app-dir-i18n/next-13-app-dir-i18n.jpg'
+              alt=''
             />
           </a>
         </div>
         <hr style={{ marginTop: 20, width: '90%' }} />
         <div>
           <Link href={`/${lng}/second-page`}>
-            <button type="button">{t('to-second-page')}</button>
+            <button type='button'>{t('to-second-page')}</button>
           </Link>
           <Link href={`/${lng}/client-page`}>
-            <button type="button">{t('to-client-page')}</button>
+            <button type='button'>{t('to-client-page')}</button>
           </Link>
         </div>
       </main>
