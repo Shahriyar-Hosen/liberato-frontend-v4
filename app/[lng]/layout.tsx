00b79dddf41f18803fdfef704c462d17afb07182
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-06 11:35:53 +0600

    setup Footer  page

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -9,7 +9,7 @@ import { IChildren, IParamsLng } from '@/types';
 import { dir } from 'i18next';
 import { ThemeProvider } from 'next-themes';
 import { languages } from '../i18n/settings';
-import { Navbar } from './components';
+import { Footer, Navbar } from './components';
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -26,6 +26,7 @@ export default function RootLayout({ children, params: { lng } }: IRootLayout) {
           <ThemeProvider enableSystem attribute='class' defaultTheme='system'>
             <Navbar lng={lng} />
             {children}
+            <Footer lng={lng} />
           </ThemeProvider>
         </main>
       </body>
